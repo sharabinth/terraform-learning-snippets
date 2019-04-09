4845c7e7ae1dd045bb82036e7beeb8924c87037e
sharabinth <maha@hashicorp.com>
2019-04-09 21:12:04 +1000

    updated

--- 1-basics/main.tf
+++ 1-basics/main.tf
@@ -6,6 +6,16 @@ provider "aws" {
 
 resource "aws_instance" "msb-web" {
   #ami           = "ami-2757f631"
-  ami           = "ami-b374d5a5"
+  #ami           = "ami-b374d5a5"
+  ami = "${lookup(var.amis,var.aws_region)}"
+
   instance_type = "t2.micro"
+
+  provisioner "local-exec" {
+    command = "echo ${aws_instance.msb-web.public_ip} > ip_address.txt"
+  }
+}
+
+resource "aws_eip" "ip" {
+  instance = "${aws_instance.msb-web.id}"
 }
--- 1-basics/vars.tf
+++ 1-basics/vars.tf
@@ -2,6 +2,15 @@ variable "AWS_ACCESS" {}
 
 variable "AWS_SECRET" {}
 
+variable "amis" {
+  type = "map"
+  default = {
+    "us-east-1" = "ami-b374d5a5"
+    "us-west-2" = "ami-4b32be2b"
+  }
+}
+
 variable "aws_region" {
   default = "us-east-1"
 }
+
