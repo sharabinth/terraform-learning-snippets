ecb73d0ec13352013761e0f513a70d8cca06a71b
sharabinth <maha@hashicorp.com>
2019-04-10 18:44:23 +1000

    remote store for state

---
+++ 3-remote-state/main.tf
@@ -0,0 +1,29 @@
+provider "aws" {
+  access_key = "${var.AWS_ACCESS}"
+  secret_key = "${var.AWS_SECRET}"
+  region     = "${var.aws_region}"
+}
+
+terraform {
+  backend "consul" {
+    address = "demo.consul.io"
+    path    = "getting-started-msb"
+    lock    = false
+  }
+}
+
+resource "aws_instance" "msb-web" {
+  #ami           = "ami-2757f631"
+  #ami           = "ami-b374d5a5"
+  ami = "${lookup(var.amis,var.aws_region)}"
+
+  instance_type = "t2.micro"
+
+  provisioner "local-exec" {
+    command = "echo ${aws_instance.msb-web.public_ip} > ip_address.txt"
+  }
+}
+
+resource "aws_eip" "ip" {
+  instance = "${aws_instance.msb-web.id}"
+}
